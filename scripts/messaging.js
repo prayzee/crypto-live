e3b033df8e37ae95794b0c885abd6f7061e5e537
Prayag <prayag1601@gmail.com>
2021-07-27 09:58:18 +1000

    make manual chat event submission listeners into a form

--- scripts/messaging.js
+++ scripts/messaging.js
@@ -1,8 +1,8 @@
 const chat = document.getElementById('chat');
+const chatForm = document.getElementById('chatForm');
 const messageForm = document.getElementById('messageToSend');
 messageForm.style.cssText = 'text-align: center;';
 messageForm.placeholder = 'Chat with other live users...';
-const sendMessageButton = document.getElementById('sendMessageButton');
 
 function displayChatMessage(message) {
     const chat = document.getElementById('chat');
@@ -15,20 +15,9 @@ function displayChatMessage(message) {
     chat.scrollTop = chat.scrollHeight;
 }
 
-// send message upon button click
-sendMessageButton.addEventListener('click', () => {
-    const text = document.getElementById('messageToSend');
-    if (text.value.length != 0) {
-        socket.send(JSON.stringify({ event: 'chatMessage', message: text.value }));
-        text.value = '';
-    }
-});
+chatForm.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-// send message upon pressing enter key
-messageForm.addEventListener('keyup', (e) => {
-    if (e.key != 'Enter') {
-        return;
-    }
     const text = document.getElementById('messageToSend');
     if (text.value.length != 0) {
         socket.send(JSON.stringify({ event: 'chatMessage', message: text.value }));
